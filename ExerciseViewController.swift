b73ef9ce9cb317f237e4c108dce17d34d23bd70d
Tristan Darwent <tristandarwent@gmail.com>
2018-11-23 21:19:00 -0500

    Initial commit

---
+++ ExerciseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExerciseViewController.swift
+//  Grip
+//
+//  Created by Tristan Darwent on 2018-11-23.
+//  Copyright © 2018 Tristan Darwent. All rights reserved.
+//
+
+import UIKit
+
+class ExerciseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
